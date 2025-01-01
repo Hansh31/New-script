d437cce08dfba591c97e8ad0258b5691c01ad6f2
Hansh31 <anilchinna13@gmail.com>
2025-01-01 13:18:42 +0530

    new code is added

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PERSON1=Ramesh
-PERSON2=Suresh
+PERSON1=$1 # no space before and after '=' symbol
+PERSON2=$2
 
 echo "$PERSON1: Hi $PERSON2"
 echo "$PERSON2: Hello Macha , how are you ?"
