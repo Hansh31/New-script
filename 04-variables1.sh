d8ed40fd31296a87e5573c892c4941b6d1a1404a
Hansh31 <anilchinna13@gmail.com>
2025-01-01 13:31:49 +0530

    new code is added

--- 04-variables1.sh
+++ 04-variables1.sh
@@ -5,3 +5,7 @@ echo "please enter your user name" # text entered here will be added as value to
 read -s USERNAME # here USERNAME is Variable name
 
 echo "user name entered: $USERNAME"
+
+echo "Please enter your password"
+
+read -s PASSWORD
