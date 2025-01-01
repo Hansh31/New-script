c4539fe13df31a1726ad4033ab476865802cea93
Hansh31 <anilchinna13@gmail.com>
2025-01-01 12:43:55 +0530

    new code is added

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "Rahim : Hi Robert"
-echo "Robert : Hello Macha , how are you ?"
-echo "Rahim: all good , how about you ?
+echo "Rahim: Hi Robert"
+echo "Robert: Hello Macha , how are you ?"
+echo "Rahim: all good , how about you ?"
