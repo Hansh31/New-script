ac9379f8d31703d4081d8ed9009d7ff41c1989c2
Hansh31 <anilchinna13@gmail.com>
2025-01-03 21:23:04 +0530

    new message

--- 12-logs.sh
+++ 12-logs.sh
@@ -6,7 +6,7 @@ G="\e[32m"
 Y="\e[33m"
 LOGS_FOLDER="/var/log/Newscript-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
-TIMESTAMP=$(date +%Y-%m-%d-%H-%N-%S)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 VALIDATE(){
